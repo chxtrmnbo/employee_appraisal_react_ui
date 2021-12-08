bef96f68c649f930d86e12ab57883b15b2da6c7c
Carl <manibocarl@gmail.com>
2021-12-09 00:54:00 +0800

    Put the commentbox from the prototype

---
+++ src/components/CommentBox.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, Form } from "react-bootstrap";
+import { useState } from "react";
+
+export default function CommentBox(props) {
+  const [comment, setComment] = useState(props.comment);
+  const handleChange = (e) => {
+    setComment(e.target.value);
+    props.commentCallback(e.target.value);
+  };
+  return (
+    <Card>
+      <Card.Header className="bg-primary fw-bold text-white text-right">
+        {props.title}
+      </Card.Header>
+      <Form.Control
+        as="textarea"
+        rows={3}
+        maxLength={255}
+        value={props.comment.comments}
+        onChange={(e) => handleChange(e)}
+      />
+    </Card>
+  );
+}
