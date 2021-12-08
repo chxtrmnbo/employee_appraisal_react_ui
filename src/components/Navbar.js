ceb37b1528a56f1f2d95978ce71318f87fa3efba
unknown <ragannaba20@gmail.com>
2021-12-08 17:48:58 +0800

    Updated the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,47 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, } from 'react-bootstrap'
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 
 const Navigation = () => {
+  const navigations = [
+    { title: "Login", route: "/Login" },
+    { title: "Supervisor Queue", route: "/Supervisor" },
+    { title: "HR Queue", route: "/Hr" },
+    { title: "Self Assessment", route: "/Assessment" },
+  ];
 
-    const navigations = [
-        { title: 'Login', route: '/Login' },
-        { title: 'Supervisor Queue', route: '/Supervisor' },
-        { title: 'HR Queue', route: '/Hr' },
-        { title: 'Self Assessment', route: '/Assessment' },
-    ]
+  return (
+    <div>
+      <Navbar style={{ backgroundColor: "#4852b0" }} variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              style={{ width: "40px" }}
+              src="https://e-learn.cisateducation.com/wp-content/uploads/2020/04/f07_db.png"
+              alt="LOGO"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="text-white">
+            <Nav className="ms-auto" style={{ color: "white" }}>
+              <NavDropdown title="Alden Recharge" id="basic-nav-dropdown">
+                {navigations.map((nav, index) => (
+                  <NavDropdown.Item
+                    href={nav.route}
+                    className="text-decoration-none text-muted px-5"
+                  >
+                    {nav.title}
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
 
-    return (
-        <Navbar bg="white" variant="light" style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}>
-            <Container>
-                <Navbar.Brand href="/">Navigation</Navbar.Brand>
-                <Nav className="me-auto">
-                    {
-                        navigations.map((nav, index) =>
-                            <Link to={nav.route} className="text-decoration-none text-muted px-5">
-                                {nav.title}
-                            </Link>
-                        )
-                    }
-                </Nav>
-            </Container>
-        </Navbar >
-    );
-}
+              <Nav.Link href="#home">
+               <span className="ms-3">LOGOUT</span>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 export default Navigation;
-
