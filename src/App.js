a1de5c2da42e488f54edabfb15333dce424c3b12
adrianvanladrillano <64422461+adrianvanladrillano@users.noreply.github.com>
2021-12-08 14:34:38 +0800

    Updated routes, navbar
    Updated routes, navbar
    Created Rfc ratee assessment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { Container } from 'react-bootstrap'
 
 //Pages
 import Login from './pages/Login'
+import SupervisorQueue from './pages/SupervisorQueue'
+import HrQueue from './pages/HrQueue'
+import RateeAssessment from './pages/RateeAssessment'
+
+// Components
 import Navigation from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +17,9 @@ function App() {
       <Container>
         <Routes>
           <Route path="/Login" element={<Login />} />
+          <Route path="/Supervisor" element={<SupervisorQueue />} />
+          <Route path="/Hr" element={<HrQueue />} />
+          <Route path="/Assessment" element={<RateeAssessment />} />
         </Routes>
       </Container>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,26 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, } from 'react-bootstrap'
 
 const Navigation = () => {
+
+    const navigations = [
+        { title: 'Login', route: '/Login' },
+        { title: 'Supervisor Queue', route: '/Supervisor' },
+        { title: 'HR Queue', route: '/Hr' },
+        { title: 'Self Assessment', route: '/Assessment' },
+    ]
+
     return (
         <Navbar bg="white" variant="light" style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}>
             <Container>
                 <Navbar.Brand href="/">Navigation</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Link to="/Login" className="text-decoration-none text-muted px-5">
-                        Login
-                    </Link>
+                    {
+                        navigations.map((nav, index) =>
+                            <Link to={nav.route} className="text-decoration-none text-muted px-5">
+                                {nav.title}
+                            </Link>
+                        )
+                    }
                 </Nav>
             </Container>
         </Navbar >
