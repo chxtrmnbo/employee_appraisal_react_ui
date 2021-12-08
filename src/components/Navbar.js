d2acfd9e25a845f338a73402c2a89c20a05f2303
adrianvanladrillano <64422461+adrianvanladrillano@users.noreply.github.com>
2021-12-08 20:04:42 +0800

    Created title component
    Created title component, fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,53 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
-  const navigations = [
-    { title: "Login", route: "/Login" },
-    { title: "Supervisor Queue", route: "/Supervisor" },
-    { title: "HR Queue", route: "/Hr" },
-    { title: "Self Assessment", route: "/Assessment" },
-  ];
+    const navigations = [
+        { title: "Login", route: "/Login" },
+        { title: "Supervisor Queue", route: "/Supervisor" },
+        { title: "HR Queue", route: "/Hr" },
+        { title: "Self Assessment", route: "/Assessment" },
+    ];
 
-  return (
-    <div>
-      <Navbar style={{ backgroundColor: "#4852b0" }} variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              style={{ width: "40px" }}
-              src="https://e-learn.cisateducation.com/wp-content/uploads/2020/04/f07_db.png"
-              alt="LOGO"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="text-white">
-            <Nav className="ms-auto" style={{ color: "white" }}>
-              <NavDropdown title="Alden Recharge" id="basic-nav-dropdown">
-                {navigations.map((nav, index) => (
-                  <NavDropdown.Item
-                    href={nav.route}
-                    className="text-decoration-none text-muted px-5"
-                  >
-                    {nav.title}
-                  </NavDropdown.Item>
-                ))}
-              </NavDropdown>
+    return (
+        <div>
+            <Navbar style={{ backgroundColor: "#4852b0" }} variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            style={{ width: "40px" }}
+                            src="https://e-learn.cisateducation.com/wp-content/uploads/2020/04/f07_db.png"
+                            alt="LOGO"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="text-white">
+                        <Nav className="ms-auto" style={{ color: "white" }}>
+                            <NavDropdown title="Alden Recharge" id="basic-nav-dropdown">
+                                {navigations.map((nav, index) => (
 
-              <Nav.Link href="#home">
-               <span className="ms-3">LOGOUT</span>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
+                                    <NavDropdown.Item
+                                        className=""
+                                    >
+                                        <Link key={index}
+                                            className="text-decoration-none text-muted "
+                                            to={nav.route}
+                                        >
+                                            {nav.title}
+                                        </Link>
+                                    </NavDropdown.Item>
+                                ))}
+                            </NavDropdown>
+
+                            <Nav.Link href="#home">
+                                <span className="ms-3">LOGOUT</span>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
 };
 export default Navigation;
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Title(props) {
     return (
-        <div className="text-center">
-            <h5>{props.title}</h5>
-            <h5> {props.subtitle}</h5>
+        <div>
+            <h2>{props.title}</h2>
+            <p className="text-muted">L{props.subtitle}</p>
         </div>
     )
 }
