e38aaf417b58c1fb3c8bd264bcc7419024780ec0
adrianvanladrillano <64422461+adrianvanladrillano@users.noreply.github.com>
2021-12-09 08:11:40 +0800

    Update storage.js

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,3 +1,14 @@
 let user = JSON.parse(localStorage.getItem('user'))
 const Auth = user;
+
+if (!Auth) {
+    Auth = {
+        name: '',
+        email: '',
+        password: '',
+        department: '',
+        role: 0
+    }
+}
+
 export default Auth;
